1768ab3d98b7d3941107e394223744a2ce9db3ff
MitchGianoni <mitchgianoni@yahoo.com>
2018-10-30 07:48:56 -0400

    added second client origin

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' || process.env.CLIENT_ORIGIN2,
   DATABASE_URL:
       process.env.DATABASE_URL || 'postgres://localhost/deckbuilder',
   TEST_DATABASE_URL:
